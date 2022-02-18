0eb311486298995afef59128ba1d3734db2a5031
Patrick Walton <pcwalton@mimiga.net>
2022-02-18 15:05:38 -0800

    Refactor the execlet code into a separate file

--- cxx-async/src/lib.rs
+++ cxx-async/src/lib.rs
@@ -135,9 +135,8 @@
 
 extern crate link_cplusplus;
 
+use crate::execlet::{Execlet, ExecletReaper, RustExeclet};
 use futures::{Stream, StreamExt};
-use once_cell::sync::OnceCell;
-use std::collections::VecDeque;
 use std::convert::From;
 use std::error::Error;
 use std::ffi::CStr;
@@ -146,9 +145,9 @@ use std::future::Future;
 use std::mem::{self, MaybeUninit};
 use std::panic::{self, AssertUnwindSafe};
 use std::pin::Pin;
-use std::sync::{Arc, Condvar, Mutex, Weak};
+use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
-use std::{ptr, thread};
+use std::ptr;
 
 const FUTURE_STATUS_PENDING: u32 = 0;
 const FUTURE_STATUS_COMPLETE: u32 = 1;
@@ -198,6 +197,9 @@ macro_rules! safe_unreachable {
         safe_panic!("unreachable code executed")
     }
 }
+
+#[doc(hidden)]
+pub mod execlet;
 
 // Bridged glue functions.
 extern "C" {
@@ -292,242 +294,6 @@ where
     // The sending end.
     sender: CxxAsyncSender<Item>,
 }
-
-// Allows the Rust polling interface to drive C++ tasks to completion.
-//
-// This is needed by the Folly backend, to allow awaiting semifutures.
-#[derive(Clone)]
-#[doc(hidden)]
-pub struct Execlet(Arc<RustExeclet>);
-
-struct WeakExeclet(Weak<RustExeclet>);
-
-// The type that C++ sees for an execlet. This is opaque as far as C++ is concerned.
-#[doc(hidden)]
-pub struct RustExeclet(Mutex<ExecletImpl>);
-
-impl Execlet {
-    // Creates a new execlet with no waker and an empty runqueue.
-    fn new() -> Execlet {
-        Execlet(Arc::new(RustExeclet(Mutex::new(ExecletImpl {
-            runqueue: VecDeque::new(),
-            waker: None,
-            running: false,
-        }))))
-    }
-
-    fn downgrade(&self) -> WeakExeclet {
-        WeakExeclet(Arc::downgrade(&self.0))
-    }
-
-    // Consumes the reference.
-    unsafe fn from_raw(ptr: *const RustExeclet) -> Execlet {
-        Execlet(Arc::from_raw(ptr))
-    }
-
-    // Bumps the reference count.
-    unsafe fn from_raw_ref(ptr: *const RustExeclet) -> Execlet {
-        let this = Execlet::from_raw(ptr);
-        mem::forget(this.clone());
-        this
-    }
-
-    // Runs all tasks in the runqueue to completion.
-    fn run(&self, cx: &mut Context) {
-        // Lock.
-        let mut guard = self.0 .0.lock().unwrap();
-        safe_debug_assert!(!guard.running);
-        guard.running = true;
-
-        // Run as many tasks as we have.
-        guard.waker = Some((*cx.waker()).clone());
-        while let Some(task) = guard.runqueue.pop_front() {
-            // Drop the lock so that the task that we run can safely enqueue new tasks without
-            // deadlocking.
-            drop(guard);
-            unsafe {
-                task.run();
-            }
-            // Re-acquire the lock.
-            guard = self.0 .0.lock().unwrap();
-        }
-
-        // Unlock.
-        guard.running = false;
-    }
-
-    // Submits a task to this execlet.
-    fn submit(&self, task: ExecletTask) {
-        let mut this = self.0 .0.lock().unwrap();
-        this.runqueue.push_back(task);
-        if !this.running {
-            if let Some(ref waker) = this.waker {
-                // Avoid possible deadlocks.
-                let waker = (*waker).clone();
-                drop(this);
-                waker.wake_by_ref();
-            }
-        }
-    }
-}
-
-// Data for each execlet.
-struct ExecletImpl {
-    // Tasks waiting to run.
-    runqueue: VecDeque<ExecletTask>,
-    // A Rust Waker that will be informed when we need to wake up and run some tasks in our
-    // runqueue or yield the result.
-    waker: Option<Waker>,
-    // True if we're running; false otherwise. This flag is necessary to avoid deadlocks resulting
-    // from recursive invocations.
-    running: bool,
-}
-
-struct ExecletReaper {
-    execlets: Mutex<ExecletReaperQueue>,
-    cond: Condvar,
-}
-
-struct ExecletReaperQueue {
-    new: Vec<WeakExeclet>,
-    old: Vec<Execlet>,
-}
-
-impl ExecletReaper {
-    fn get() -> Arc<ExecletReaper> {
-        static INSTANCE: OnceCell<Arc<ExecletReaper>> = OnceCell::new();
-        (*INSTANCE.get_or_init(|| {
-            let reaper = Arc::new(ExecletReaper {
-                execlets: Mutex::new(ExecletReaperQueue {
-                    new: vec![],
-                    old: vec![],
-                }),
-                cond: Condvar::new(),
-            });
-            let reaper_ = reaper.clone();
-            thread::spawn(move || ExecletReaper::run(reaper_));
-            reaper
-        }))
-        .clone()
-    }
-
-    fn add(&self, execlet: Execlet) {
-        let mut execlets = self.execlets.lock().unwrap();
-        execlets.new.push(execlet.downgrade());
-
-        // Go ahead and start running the execlet if the reaper is sleeping. This makes sure that
-        // we properly handle the following sequence of events:
-        //
-        // 1. User code drops the `CxxAsyncReceiver` from a task T and `CxxAsyncReceiver::drop()`
-        //    starts running, but doesn't get here yet.
-        // 2. The wrapped C++ future resolves on some C++ executor on another thread and submits a
-        //    task U to the execlet.
-        // 3. The original Waker is invoked and the Rust executor makes a note to resume task T.
-        //    Because T is already running and has dropped the receiver, this does *not* cause the
-        //    execlet to run.
-        // 4. We add the execlet to this reaper.
-        //
-        // In this case, we have to make sure that we run task U, even though an execlet reaper
-        // waker was never invoked.
-        self.cond.notify_all();
-    }
-
-    fn run(self: Arc<Self>) {
-        loop {
-            let mut execlets = self.execlets.lock().unwrap();
-            while execlets.is_empty() {
-                execlets = self.cond.wait(execlets).unwrap();
-            }
-
-            let execlets_to_process = execlets.take();
-            for execlet in execlets_to_process {
-                drop(execlets);
-                unsafe {
-                    execlet.run(&mut Context::from_waker(&Waker::from_raw(
-                        ExecletReaperWaker {
-                            execlet: execlet.clone(),
-                        }
-                        .into_raw(),
-                    )));
-                }
-                execlets = self.execlets.lock().unwrap();
-                execlets.old.push(execlet);
-            }
-        }
-    }
-
-    fn wake(&self) {
-        self.cond.notify_all();
-    }
-}
-
-impl ExecletReaperQueue {
-    fn is_empty(&self) -> bool {
-        self.new.is_empty() && self.old.is_empty()
-    }
-
-    fn take(&mut self) -> Vec<Execlet> {
-        let mut execlets = mem::take(&mut self.old);
-        for execlet in mem::take(&mut self.new).into_iter() {
-            if let Some(execlet) = execlet.0.upgrade() {
-                execlets.push(Execlet(execlet));
-            }
-        }
-        execlets
-    }
-}
-
-#[derive(Clone)]
-struct ExecletReaperWaker {
-    execlet: Execlet,
-}
-
-impl ExecletReaperWaker {
-    unsafe fn from_raw(data: *const ()) -> ExecletReaperWaker {
-        ExecletReaperWaker {
-            execlet: Execlet(Arc::from_raw(data as *const RustExeclet)),
-        }
-    }
-
-    fn into_raw(self) -> RawWaker {
-        RawWaker::new(
-            Arc::into_raw(self.execlet.0) as *const (),
-            &EXECLET_REAPER_WAKER_VTABLE,
-        )
-    }
-
-    fn wake(self) {
-        ExecletReaper::get().wake();
-    }
-}
-
-unsafe fn execlet_reaper_waker_clone(data: *const ()) -> RawWaker {
-    let execlet_reaper_waker = ExecletReaperWaker::from_raw(data);
-    let waker = execlet_reaper_waker.clone().into_raw();
-    mem::forget(execlet_reaper_waker);
-    waker
-}
-
-unsafe fn execlet_reaper_waker_wake(data: *const ()) {
-    ExecletReaperWaker::from_raw(data).wake()
-}
-
-unsafe fn execlet_reaper_waker_wake_by_ref(data: *const ()) {
-    let waker = ExecletReaperWaker::from_raw(data);
-    waker.clone().wake();
-    mem::forget(waker);
-}
-
-unsafe fn execlet_reaper_waker_drop(data: *const ()) {
-    drop(ExecletReaperWaker::from_raw(data))
-}
-
-static EXECLET_REAPER_WAKER_VTABLE: RawWakerVTable = RawWakerVTable::new(
-    execlet_reaper_waker_clone,
-    execlet_reaper_waker_wake,
-    execlet_reaper_waker_wake_by_ref,
-    execlet_reaper_waker_drop,
-);
 
 // The single-producer/single-consumer channel type that future and stream implementations use to
 // pass values between the two languages.
@@ -764,29 +530,6 @@ pub trait RustSender {
     type Output;
     fn send(&mut self, value: CxxAsyncResult<Self::Output>);
 }
-
-// A continuation in an execlet's run queue.
-struct ExecletTask {
-    // A C++ stub that resumes this task.
-    run: unsafe extern "C" fn(*mut u8),
-    // The task data, passed to `run`.
-    data: *mut u8,
-}
-
-impl ExecletTask {
-    // Creates a new `ExecletTask`.
-    fn new(run: unsafe extern "C" fn(*mut u8), data: *mut u8) -> Self {
-        Self { run, data }
-    }
-
-    // Resumes the task.
-    unsafe fn run(self) {
-        (self.run)(self.data)
-    }
-}
-
-unsafe impl Send for ExecletTask {}
-unsafe impl Sync for ExecletTask {}
 
 /// Wraps an arbitrary Rust Future in a boxed `cxx-async` future so that it can be returned to C++.
 ///
@@ -1098,33 +841,6 @@ unsafe fn rust_suspended_coroutine_wake_by_ref(address: *const ()) {
 // SAFETY: This is a raw FFI function called by the currently-running Rust executor.
 unsafe fn rust_suspended_coroutine_drop(address: *const ()) {
     cxxasync_suspended_coroutine_drop(address as *mut () as *mut u8)
-}
-
-// Execlet FFI
-
-// C++ calls this to create a new execlet.
-#[no_mangle]
-#[doc(hidden)]
-pub unsafe extern "C" fn cxxasync_execlet_create() -> *const RustExeclet {
-    Arc::into_raw(Execlet::new().0)
-}
-
-// C++ calls this to decrement the reference count on an execlet and free it if the count hits zero.
-#[no_mangle]
-#[doc(hidden)]
-pub unsafe extern "C" fn cxxasync_execlet_release(this: *mut RustExeclet) {
-    drop(Execlet::from_raw(this))
-}
-
-// C++ calls this to submit a task to the execlet. This internally bumps the reference count.
-#[no_mangle]
-#[doc(hidden)]
-pub unsafe extern "C" fn cxxasync_execlet_submit(
-    this: *mut RustExeclet,
-    run: extern "C" fn(*mut u8),
-    task_data: *mut u8,
-) {
-    Execlet::from_raw_ref(this).submit(ExecletTask::new(run, task_data))
 }
 
 // Reexports for the `#[bridge]` macro to use internally. Users of this crate shouldn't use these;
