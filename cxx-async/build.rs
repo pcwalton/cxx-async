cf53fba094b2ce89f9254cf473442725e1694ce4
Patrick Walton <pcwalton@mimiga.net>
2022-08-19 20:07:54 -0700

    Pass the path to the directory that includes `cxx_async.h` to downstream crates.
    This makes the `cxx-async` crate friendlier to users.

--- cxx-async/build.rs
+++ cxx-async/build.rs
@@ -1,8 +1,29 @@
 // cxx-async/build.rs
 
-use std::path::PathBuf;
+use std::env;
+use std::fs;
+use std::path::{Path, PathBuf};
 
 fn main() {
+    // Copy include files so that dependent crates can find them.
+
+    let out_dir = Path::new(&env::var("OUT_DIR").expect("`OUT_DIR` not set!")).to_owned();
+    let mut src_include_path = Path::new(".").to_owned();
+    let mut dest_include_path = out_dir.clone();
+    for include_path in &mut [&mut src_include_path, &mut dest_include_path] {
+        include_path.push("include");
+        include_path.push("rust");
+    }
+
+    drop(fs::create_dir_all(&dest_include_path));
+    for header in &["cxx_async.h", "cxx_async_cppcoro.h", "cxx_async_folly.h"] {
+        drop(fs::copy(
+            Path::join(&src_include_path, header),
+            Path::join(&dest_include_path, header),
+        ));
+    }
+    println!("cargo:include={}", Path::join(&out_dir, "include").to_string_lossy());
+
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=src/cxx_async.cpp");
     println!("cargo:rerun-if-changed=include/rust/cxx_async.h");
